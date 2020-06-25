0a1241e4a1ef6f2d5acbe34c2e477dd07f2c95a1
Ashwin Philip <ashphil30@gmail.com>
2020-06-25 18:05:00 -0400

    "Question 1"

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,15 +3,28 @@ import './styles/ListItem.css';
 
 const ListItem = ({ item, row }) => {
 	const { name, uuid, description, price, partner } = item;
+	function classNames() {
+		if(row % 2 == 0)
+		{
+			return "evenClassName";
+		}
+
+		else
+		{
+			return "oddClassName";
+		}
+	}
 	return (
 		<tr>
-			<td><p>{name}</p></td>
-      <td>{uuid}</td>
-      <td>{description}</td>
-      <td>{price}</td>
-      <td>{partner}</td>
+			<td className={classNames()}><p>{name}</p></td>
+			<td className={classNames()}>{uuid}</td>
+			<td className={classNames()}>{description}</td>
+			<td className={classNames()}>{price}</td>
+			<td className={classNames()}>{partner}</td>
 		</tr>
 	);
+
+
 };
 
 export default ListItem;
