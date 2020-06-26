6e5e8d72c4c5bb35b63ae9e3e0ac98f713029448
ted-han <taekyung28h@gmail.com>
2020-06-26 16:30:51 -0400

    Fix price patten

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React from 'react';
 import './styles/ListItem.css';
 
 const ListItem = ({ item, row }) => {
-	const { name, uuid, description, price, partner } = item;
-	return (
-		<tr>
-			<td><p>{name}</p></td>
+  const { name, uuid, description, price, partner } = item;
+  let fixPrice = price ? `$${parseInt(price).toFixed(2)}` : '';
+  return (
+    <tr>
+      <td>
+        <p>{name}</p>
+      </td>
       <td>{uuid}</td>
       <td>{description}</td>
-      <td>{price}</td>
+      <td>{fixPrice}</td>
       <td>{partner}</td>
-		</tr>
-	);
+    </tr>
+  );
 };
 
 export default ListItem;
