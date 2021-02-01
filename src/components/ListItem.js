91655c925f8129c3f9517026e2029d24971f5536
Pat Steeves <patricksteeves02@gmail.com>
2021-02-01 13:09:04 -0500

    alternating colors in table

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,8 +3,11 @@ import './styles/ListItem.css';
 
 const ListItem = ({ item, row }) => {
 	const { name, uuid, description, price, partner } = item;
+	// Whether row is odd or even. Used to style table with
+	// alternating colors.
+	const className = `row${row % 2}`
 	return (
-		<tr>
+		<tr className={className}>
 			<td><p>{name}</p></td>
       <td>{uuid}</td>
       <td>{description}</td>
